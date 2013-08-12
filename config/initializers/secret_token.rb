3bf45894f5ca6caef4a641c134d51811ade1b1e3
Christian Hernandez <chernandez1982@yahoo.com>
2013-08-12 09:55:13 -0700

    Udated Security Token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,6 +7,21 @@
 # no regular words or you'll be exposed to dictionary attacks.
 # You can use `rake secret` to generate a secure secret key.
 
+## Randomly generate token
+require 'securerandom'
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use existing token
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in the "token_file"
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Microblog::Application.config.secret_key_base = '3028a30af701b8551724860b2b9b3d8fcf280f9023f5c55cab1c2267a7180287d4c38fc1906b2318b734535be3a53fff77acf119aafa92fb708ecfb268d96493'
+Microblog::Application.config.secret_key_base = secure_token
