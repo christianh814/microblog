d49f409c8f8c8880923af7edf06832a5c1826b7b
root <root@fedorarails.default>
2013-08-12 09:08:45 -0700

    Installed basic rails framework. First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Microblog::Application.config.secret_key_base = '3028a30af701b8551724860b2b9b3d8fcf280f9023f5c55cab1c2267a7180287d4c38fc1906b2318b734535be3a53fff77acf119aafa92fb708ecfb268d96493'
