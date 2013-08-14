f94618c89eb79abf1b87a03968d0df54ae6b916e
Christian Hernandez <chernandez1982@yahoo.com>
2013-08-14 16:44:43 -0700

    Finished signin module

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -34,8 +34,12 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign Out', href: signout_path) }
       it { should_not have_link('Sign In', href: signin_path)}
-    end
 
+      describe "followed by sign out" do
+        before { click_link "Sign Out" }
+	it { should have_link('Sign In') }
+      end
+    end
   end
 
 end
