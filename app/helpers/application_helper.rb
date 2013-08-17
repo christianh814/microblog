c43e1ca95995d41b9f3cd2f6eeaf740115811eb6
Christian Hernandez <chernandez1982@yahoo.com>
2013-08-17 10:03:55 -0700

    Changed application_helper variable to no be so verbose

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "Ruby On Rails Tutorial Microblog"
+    base_title = "Microblog"
     if page_title.empty?
       base_title
     else
