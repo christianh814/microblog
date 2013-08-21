53f8b6333484ae1788a3d2182cd482c9a0ac0003
Christian Hernandez <chernandez1982@yahoo.com>
2013-08-21 10:51:08 -0700

    Added REGEX for @ replies :: PREPARATION

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,7 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates :content, presence: true, length: { maximum: 160 }
   validates :user_id, presence: true
+  AT_REPLY_REGEX = /(@)\S+/i
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
     where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id)
