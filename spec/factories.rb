6896949d2a6ee66f97d41081270f003f7acc3537
Christian Hernandez <chernandez1982@yahoo.com>
2013-08-22 11:33:26 -0700

    Cleaning up HTML on (mostly) static pages

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :user do
     sequence(:name) { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.net" }
+    sequence(:username) { |n| "person#{n}ex" }
     password			"foobar"
     password_confirmation	"foobar"
 
